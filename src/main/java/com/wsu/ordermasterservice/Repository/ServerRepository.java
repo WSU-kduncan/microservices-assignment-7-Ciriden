5bdf0a6eb98ab74cf711a32e522895e789931f89
Denver Woolard <112095324+Ciriden@users.noreply.github.com>
2024-10-23 19:46:42 -0400

    Update ServerRepository.java

--- src/main/java/com/wsu/ordermasterservice/Repository/ServerRepository.java
+++ src/main/java/com/wsu/ordermasterservice/Repository/ServerRepository.java
@@ -4,8 +4,6 @@ import com.example.servers.model.Server;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Repository interface for Server entity
-@Repository
 public interface ServerRepository extends JpaRepository<Server, Integer> {
-    // Built-in methods like save, deleteById, findById can be leveraged
+
 }
