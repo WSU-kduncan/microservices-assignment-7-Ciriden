61510bff82c035c7b96a351390a5ad4d45686694
Denver Woolard <112095324+Ciriden@users.noreply.github.com>
2024-10-23 19:50:50 -0400

    Update ServerDTO.java

--- src/main/java/com/wsu/ordermasterservice/DTO/ServerDTO.java
+++ src/main/java/com/wsu/ordermasterservice/DTO/ServerDTO.java
@@ -1,19 +1,33 @@
 package com.wsu.ordermasterservice.DTO;
 
-import javax.validation.constraints.NotBlank;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-// Data Transfer Object for Server
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ServerDTO {
 
-    private int serverId;
+    private Integer serverId;
 
     @NotBlank(message = "First name is required")
+    @Size(max = 20)
     private String firstName;
 
     @NotBlank(message = "Last name is required")
+    @Size(max = 25)
     private String lastName;
 
+    
     private String availability;
 
-    // Getters and setters
+
 }
