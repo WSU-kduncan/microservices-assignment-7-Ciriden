550b7ddd0290af377506e303f1c64663643b5426
Denver Woolard <112095324+Ciriden@users.noreply.github.com>
2024-10-23 19:49:19 -0400

    Update Server.java

--- src/main/java/com/wsu/ordermasterservice/Model/Server.java
+++ src/main/java/com/wsu/ordermasterservice/Model/Server.java
@@ -1,19 +1,38 @@
 package com.wsu.ordermasterservice.Model;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-// Entity class for Server
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 @Table(name = "servers")
 public class Server {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "server_id")
     private int serverId;
 
+    @Column(name = "server_first_Name")
     private String firstName;
+
+    @Column(name = "server_last_Name")
     private String lastName;
+
+    @Column(name = "server_availibility")
     private String availability;
 
-    // Getters and setters
 }
