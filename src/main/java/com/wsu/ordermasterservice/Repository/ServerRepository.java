b8a2dedf16261c25c65c8b1fe1e73cc0a306c444
Denver Woolard <112095324+Ciriden@users.noreply.github.com>
2024-10-23 20:14:25 -0400

    Update ServerRepository.java

--- src/main/java/com/wsu/ordermasterservice/Repository/ServerRepository.java
+++ src/main/java/com/wsu/ordermasterservice/Repository/ServerRepository.java
@@ -1,6 +1,6 @@
 package com.wsu.ordermasterservice.repository;
 
-import com.example.servers.model.Server;
+import com.wsu.ordermasterservice.model.Server;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
